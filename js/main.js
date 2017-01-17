d04fbaf376329386265bcf23843942ad6b2efbfc
michpenn <michpenn@icloud.com>
2017-01-17 07:36:37 -0800

    first commit of project files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by michpenn on 1/17/17.
+ */
+$(document).ready(function(){
+    slideInPyramidParts('#pyramidBottom', '#pyramidMiddle', '#pyramidTop');
+});
+
+function slideInPyramidParts(){
+    var elements = arguments;
+
+    for(var i in elements) {
+
+        setTimeout(function(x){
+            return function(){
+                var el = elements[x];
+                slideInPyramidPart(el);
+            };
+        }(i),1000*i);
+    }
+}
+
+function slideInPyramidPart(element){
+    var $el = $(element);
+    return $el.fadeIn(1000,'easeInSine');
+}
